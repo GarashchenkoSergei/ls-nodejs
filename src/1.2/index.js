e3fb7251c58a92bc494dfe2e421dc6a0649ed983
Sergei Garashchenko <sergei_garashchenko@epam.com>
2021-12-03 18:54:18 +0300

    fixes for second task

--- src/1.2/index.js
+++ src/1.2/index.js
@@ -2,41 +2,38 @@ const express = require('express');
 
 const app = express();
 const PORT = '3000';
+process.env.TIME_INTERVAL_SEC = 1;
+process.env.SHUTDOWN_INTERVAL_SEC = 30;
 
-let connections = [];
+const timeIntervalMsec = process.env.TIME_INTERVAL_SEC * 1000;
+const shutdownIntervalMsec = process.env.SHUTDOWN_INTERVAL_SEC * 1000;
+
+const createTimer = (req, res, next) => {
+  const startTime = new Date();
+  const startTimeInMsec = startTime.getTime();
+  const finishtime = startTimeInMsec + shutdownIntervalMsec;
+  let counter = startTimeInMsec;
+
+  const timeout = setTimeout(function run () {
+    counter = counter + timeIntervalMsec;
+    console.log(new Date(counter).toUTCString());
+    if (counter > finishtime) {
+      res.write('END\n');
+      res.end();
+
+      return clearTimeout(timeout);
+    }
+
+    res.write(`${new Date(counter).toUTCString()}\n`);
+    setTimeout(run, timeIntervalMsec);
+  }, timeIntervalMsec);
+};
 
 app.get('/date', (req, res, next) => {
   res.setHeader('Content-Type', 'text/html: charset=utf-8');
   res.setHeader('Transfer-Encoding', 'chunked');
-  connections.push(res);
+  createTimer(req, res, next);
 });
-
-const timeIntervalMsec = process.argv[2] * 1000;
-const shutdownIntervalMsec = process.argv[3] * 1000;
-
-let startTime = new Date();
-const startTimeInMsec = startTime.getTime();
-const finishtime = startTimeInMsec + shutdownIntervalMsec;
-let counter = startTimeInMsec;
-
-setTimeout(function run () {
-  counter = counter + timeIntervalMsec;
-  console.log(new Date(counter).toUTCString());
-  if (counter > finishtime) {
-    connections.map(res => {
-      res.write('END\n');
-
-      return res.end();
-    });
-    connections = [];
-    startTime = 0;
-  }
-  connections.map((res, i) => {
-    return res.write(`${new Date(counter).toUTCString()}\n`);
-  });
-
-  setTimeout(run, timeIntervalMsec);
-}, timeIntervalMsec);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
